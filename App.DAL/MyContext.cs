aa3f43f489a481892d5e8cc882b0cf8961d56c90
onurysr <onurysr97@gmail.com>
2022-06-03 15:32:37 +0300

    Add project files.

---
+++ App.DAL/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using App.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DAL
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options)
+        {
+
+        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.UseIdentityColumns();
+        //}
+
+        public DbSet<Ders> Dersler { get; set; }
+
+        public DbSet<Ogrenci> Ogrenciler { get; set; }
+    }
+}
